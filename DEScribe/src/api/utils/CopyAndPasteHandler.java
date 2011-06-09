5c8104274c5b6944ce6b059ef8f482cf1e07a579
sebastien.faure3 <sebastien.faure3@gmail.com>
2011-06-09 19:27:19 +0000

    - Bug fixes for CopyAndPaste Handler.

--- DEScribe/src/api/utils/CopyAndPasteHandler.java
+++ DEScribe/src/api/utils/CopyAndPasteHandler.java
@@ -10,6 +10,8 @@ import java.awt.Image;
 import java.awt.Toolkit;
 import java.awt.datatransfer.Clipboard;
 import java.awt.datatransfer.DataFlavor;
+import java.awt.datatransfer.FlavorEvent;
+import java.awt.datatransfer.FlavorListener;
 import java.awt.datatransfer.Transferable;
 import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
@@ -27,12 +29,40 @@ public class CopyAndPasteHandler {
 
     private Transferable oldContents=null;
     private String contentsType="";
-
+    
     private static CopyAndPasteHandler instance;
 
+    private Clipboard systemClipboard;
     public CopyAndPasteHandler(){
         // get the system clipboard
-        Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+        systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
+/*
+ *      //Can't use it! Only detects types' change and not contents changes (e.g. a text to another text not detected).
+        systemClipboard.addFlavorListener(new FlavorListener() {
+
+                public void flavorsChanged(FlavorEvent e) {
+                            updateContents(systemClipboard.getContents(null));
+                            System.out.println("Copy detected!");
+                            if (oldContents.isDataFlavorSupported(DataFlavor.imageFlavor)) {
+                                System.out.println("image");
+                                //AskFrame.getTheFrame().showTheFrame("What did you want to do with this picture?");
+                                AskFrame.getTheFrame().askQuestionWithRule("copyImage");
+                            } else if (oldContents.isDataFlavorSupported(DataFlavor.javaFileListFlavor)) {
+                                System.out.println("fichiers");
+                                //AskFrame.getTheFrame().showTheFrame("What did you want to do with this(thise) file(s)?");
+                                AskFrame.getTheFrame().askQuestionWithRule("copyFile");
+                            } else if (oldContents.isDataFlavorSupported(DataFlavor.stringFlavor)) {
+                                System.out.println("text");
+                                //AskFrame.getTheFrame().showTheFrame("What did you want to do with this text?");
+                                AskFrame.getTheFrame().askQuestionWithRule("copyText");
+                            } else if (contentsType.equals("unsupported")) {
+                                System.out.println("unsupported");
+                                //AskFrame.getTheFrame().showTheFrame("What did you want to do with this text?");
+                                AskFrame.getTheFrame().askQuestionWithRule("copy");
+                            }
+
+            }}
+        );*/
         updateContents(systemClipboard.getContents(null));
         scrutation();
     }
@@ -50,23 +80,74 @@ public class CopyAndPasteHandler {
         while (true){
             try {
                 if (isNewCopyDone()) {
-                    // Ask a new question - Only for test. To be generalized with rules in form.xml
+                    DataFlavor t[]=systemClipboard.getAvailableDataFlavors();
+                    
+                    /* 2
+                    if (systemClipboard.getAvailableDataFlavors().length>0){
+                        System.out.println(t[0].getHumanPresentableName());
+                        javax.swing.JOptionPane.showMessageDialog(null, t[0].getHumanPresentableName());
+
+                        // Ask a new question - Only for test. To be generalized with rules in form.xml
+                        if ((t[0].getHumanPresentableName().equals("image/x-java-image")) || (t[0].getHumanPresentableName().equals("application/pdf"))){
+                            System.out.println("image");
+                            //AskFrame.getTheFrame().showTheFrame("What did you want to do with this picture?");
+                            AskFrame.getTheFrame().askQuestionWithRule("copyImage");
+                        } else if ((t[0].getHumanPresentableName().equals("application/x-java-file-list")) ||(t[0].getHumanPresentableName().equals("application/x-java-url"))){
+                            System.out.println("fichiers");
+                            //AskFrame.getTheFrame().showTheFrame("What did you want to do with this(thise) file(s)?");
+                            AskFrame.getTheFrame().askQuestionWithRule("copyFile");
+                        } else if ((t[0].getHumanPresentableName().equals("application/x-java-text-encoding") ) || (t[0].getHumanPresentableName().equals("Unicode String")) || (t[0].getHumanPresentableName().equals("txt/rtf"))){
+                            System.out.println("text");
+                            //AskFrame.getTheFrame().showTheFrame("What did you want to do with this text?");
+                            AskFrame.getTheFrame().askQuestionWithRule("copyText");
+                        } else {
+                            System.out.println("unsupported");
+                            //AskFrame.getTheFrame().showTheFrame("What did you want to do with this text?");
+                            AskFrame.getTheFrame().askQuestionWithRule("copy");
+                        }
+                    }*/
+
+                    if (systemClipboard.getAvailableDataFlavors().length>0){
+
+                        // Ask a new question - Only for test. To be generalized with rules in form.xml
+                        if (isImageTransferable(oldContents)){
+                            //AskFrame.getTheFrame().showTheFrame("What did you want to do with this picture?");
+                            AskFrame.getTheFrame().askQuestionWithRule("copyImage");
+                        } else if (isFileTransferable(oldContents)){
+                            //AskFrame.getTheFrame().showTheFrame("What did you want to do with this(thise) file(s)?");
+                            AskFrame.getTheFrame().askQuestionWithRule("copyFile");
+                        } else if (isTextTransferable(oldContents)){
+                            //AskFrame.getTheFrame().showTheFrame("What did you want to do with this text?");
+                            AskFrame.getTheFrame().askQuestionWithRule("copyText");
+                        } else {
+                            System.out.println("unsupported");
+                            //AskFrame.getTheFrame().showTheFrame("What did you want to do with this text?");
+                            AskFrame.getTheFrame().askQuestionWithRule("copy");
+                        }
+                    }
+
+                    /*
+                    // Ask a new question   1
                     if (oldContents.isDataFlavorSupported(DataFlavor.imageFlavor)){
+                        System.out.println("image");
                         //AskFrame.getTheFrame().showTheFrame("What did you want to do with this picture?");
                         AskFrame.getTheFrame().askQuestionWithRule("copyImage");
                     } else if (oldContents.isDataFlavorSupported(DataFlavor.javaFileListFlavor)){
+                        System.out.println("fichiers");
                         //AskFrame.getTheFrame().showTheFrame("What did you want to do with this(thise) file(s)?");
                         AskFrame.getTheFrame().askQuestionWithRule("copyFile");
                     } else if (oldContents.isDataFlavorSupported(DataFlavor.stringFlavor)){
+                        System.out.println("text");
                         //AskFrame.getTheFrame().showTheFrame("What did you want to do with this text?");
                         AskFrame.getTheFrame().askQuestionWithRule("copyText");
                     } else if (contentsType.equals("unsupported")){
+                        System.out.println("unsupported");
                         //AskFrame.getTheFrame().showTheFrame("What did you want to do with this text?");
                         AskFrame.getTheFrame().askQuestionWithRule("copy");
-                    }
+                    }*/
                 }
                 try {
-                    Thread.sleep(1000);
+                    Thread.sleep(800);
                 } catch (InterruptedException ex) {
                     Logger.getLogger(CopyAndPasteHandler.class.getName()).log(Level.SEVERE, null, ex);
                 }
@@ -80,7 +161,7 @@ public class CopyAndPasteHandler {
     public void updateContents(Transferable newContents){
         oldContents=newContents;
         if (oldContents != null){
-            if (oldContents.isDataFlavorSupported(DataFlavor.imageFlavor)){
+        /*    if (oldContents.isDataFlavorSupported(DataFlavor.imageFlavor)){
                 // Image
                 contentsType="image";
             } else if (oldContents.isDataFlavorSupported(DataFlavor.javaFileListFlavor)){
@@ -89,13 +170,36 @@ public class CopyAndPasteHandler {
             } else if (oldContents.isDataFlavorSupported(DataFlavor.stringFlavor)){
                 // Texte
                 contentsType="text";
-            }/* else {
+            }*//* else {
                 // Unsupported
                 contentsType="unsupported";
             }*/
-        } else {
+        /*} else {
             contentsType="void";
+         */
+
+            DataFlavor t[]=systemClipboard.getAvailableDataFlavors();
+            if (systemClipboard.getAvailableDataFlavors().length>0){
+                    // Ask a new question - Only for test. To be generalized with rules in form.xml
+                    if (isImageTransferable(oldContents)){
+                        contentsType="image";
+                    } else if (isFileTransferable(oldContents)){
+                        contentsType="file";
+                    } else if (isTextTransferable(oldContents)){
+                        contentsType="text";
+                    } else {
+                        // Unsupported
+                        contentsType="unsupported";
+                    }
+            } else {
+                    contentsType="void";
+            }
+
+
+        } else {
+                contentsType="void";
         }
+
     }
 
     /* Check if the contents of clipBoard changed since last call, in other
@@ -103,18 +207,22 @@ public class CopyAndPasteHandler {
     */
     public Boolean isNewCopyDone() throws IOException {
 
-      // get the system clipboard
-      Clipboard systemClipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 
-      // get the contents on the clipboard in a
-      // Transferable object
-      Transferable clipboardContents = systemClipboard.getContents(null);
+        Transferable clipboardContents;
+
+        try {
+            clipboardContents = systemClipboard.getContents(null);
+        } catch (Exception IllegalStateException){
+            clipboardContents=null;
+        }
         try {
             // check if contents are empty, if so, return null
             if ((clipboardContents == null) || (isEqual(clipboardContents, oldContents))) {
+                System.out.println("5");
                 return false;
             } else {
                 // New contents for clipboard
+                System.out.println("6");
                 updateContents(clipboardContents);
                 return true;
             }
@@ -125,24 +233,99 @@ public class CopyAndPasteHandler {
      }
 
     public Boolean isEqual(Transferable tr1, Transferable tr2) throws UnsupportedFlavorException, IOException{
-        if (tr1.isDataFlavorSupported(DataFlavor.imageFlavor) && tr2.isDataFlavorSupported(DataFlavor.imageFlavor)){
-            if ((((Image) tr1.getTransferData(DataFlavor.imageFlavor)).getWidth(null)==((Image) tr2.getTransferData(DataFlavor.imageFlavor)).getWidth(null)) && (((Image) tr1.getTransferData(DataFlavor.imageFlavor)).getHeight(null)==((Image) tr2.getTransferData(DataFlavor.imageFlavor)).getHeight(null)))
-                return true;
-            else
+       if ((tr1.getTransferDataFlavors().length>0) && (tr2.getTransferDataFlavors().length>0)){
+            /*if (tr1.isDataFlavorSupported(DataFlavor.imageFlavor) && tr2.isDataFlavorSupported(DataFlavor.imageFlavor)){
+                if ((((Image) tr1.getTransferData(DataFlavor.imageFlavor)).getWidth(null)==((Image) tr2.getTransferData(DataFlavor.imageFlavor)).getWidth(null)) && (((Image) tr1.getTransferData(DataFlavor.imageFlavor)).getHeight(null)==((Image) tr2.getTransferData(DataFlavor.imageFlavor)).getHeight(null)))
+                    return true;
+                else
+                    return false;
+            } else if (tr1.isDataFlavorSupported(DataFlavor.stringFlavor) && tr2.isDataFlavorSupported(DataFlavor.stringFlavor)){
+                if (tr1.getTransferData(DataFlavor.stringFlavor).toString().equals(tr2.getTransferData(DataFlavor.stringFlavor).toString()))
+                    return true;
+                else
+                    return false;
+            } else if (tr1.isDataFlavorSupported(DataFlavor.javaFileListFlavor) && tr2.isDataFlavorSupported(DataFlavor.javaFileListFlavor)){
+                if (tr1.getTransferData(DataFlavor.javaFileListFlavor).toString().equals(tr2.getTransferData(DataFlavor.javaFileListFlavor).toString()))
+                    return true;
+                else
+                    return false;
+            } else {
                 return false;
-        } else if (tr1.isDataFlavorSupported(DataFlavor.stringFlavor) && tr2.isDataFlavorSupported(DataFlavor.stringFlavor)){
-            if (tr1.getTransferData(DataFlavor.stringFlavor).toString().equals(tr2.getTransferData(DataFlavor.stringFlavor).toString()))
-                return true;
-            else
-                return false;
-        } else if (tr1.isDataFlavorSupported(DataFlavor.javaFileListFlavor) && tr2.isDataFlavorSupported(DataFlavor.javaFileListFlavor)){
-            if (tr1.getTransferData(DataFlavor.javaFileListFlavor).toString().equals(tr2.getTransferData(DataFlavor.javaFileListFlavor).toString()))
-                return true;
-            else
-                return false;
-        } else {
-            return false;
+            }*/
+
+            if (isImageTransferable(tr1) && isImageTransferable(tr2)){
+                            System.out.println("4");
+                if ((((Image) tr1.getTransferData(DataFlavor.imageFlavor)).getWidth(null)==((Image) tr2.getTransferData(DataFlavor.imageFlavor)).getWidth(null)) && (((Image) tr1.getTransferData(DataFlavor.imageFlavor)).getHeight(null)==((Image) tr2.getTransferData(DataFlavor.imageFlavor)).getHeight(null)))
+                    return true;
+                else
+                    return false;
+            } else if (isTextTransferable(tr1) && isTextTransferable(tr2)){
+                if (tr1.getTransferData(DataFlavor.stringFlavor).toString().equals(tr2.getTransferData(DataFlavor.stringFlavor).toString()))
+                    return true;
+                else
+                    return false;
+            } else if (isFileTransferable(tr1) && isFileTransferable(tr2)){
+                    if (tr1.getTransferData(DataFlavor.javaFileListFlavor).toString().equals(tr2.getTransferData(DataFlavor.javaFileListFlavor).toString()))
+                        return true;
+                    else
+                        return false;
+
+            } else {
+                if (!isTextTransferable(tr1) && !isFileTransferable(tr1) && !isImageTransferable(tr1) && !isTextTransferable(tr2) && !isFileTransferable(tr2) && !isImageTransferable(tr2)){
+                    return true;
+                } else {
+                    return false;
+                }
+            }
+
+        } else if ((tr1.getTransferDataFlavors().length==0) && (tr2.getTransferDataFlavors().length==0)){
+            System.out.println("1");
+            return true;
+        } else if (contentsType.equals("unsupported")){            System.out.println("2");
+            return true;
+       } else {
+                       System.out.println("3");
+           return false;
         }
+
+    }
+
+    public Boolean isImageTransferable(Transferable tr){
+        DataFlavor t[]=tr.getTransferDataFlavors();
+        if (t.length>0){
+            if (api.utils.getOs.isWindows())
+                return (t[0].getHumanPresentableName().equals("IMAGE/x-java-image"));
+            else
+                return (t[0].getHumanPresentableName().contains("image"));
+        }
+        else
+            return false;
+    }
+
+    public Boolean isTextTransferable(Transferable tr){
+        DataFlavor t[]=tr.getTransferDataFlavors();
+        if (t.length>0){
+            if (api.utils.getOs.isWindows())
+                return (t[0].getHumanPresentableName().equals("application/x-java-text-encoding") || (t[0].getHumanPresentableName().equals("application/x-java-url")));
+            else {
+                return ((t[0].getHumanPresentableName().equals("Unicode String")) || (t[0].getHumanPresentableName().equals("text/rtf")) || (t[0].getHumanPresentableName().equals("application/pdf")));
+            }
+                
+        }
+        else
+            return false;
+    }
+
+    public Boolean isFileTransferable(Transferable tr){
+        DataFlavor t[]=tr.getTransferDataFlavors();
+        if (t.length>0){
+            if (api.utils.getOs.isWindows())
+                return (t[0].getHumanPresentableName().equals("application/x-java-file-list"));
+            else
+                return (t[0].getHumanPresentableName().equals("application/x-java-url"));
+        }
+        else
+            return false;
     }
 
 }
