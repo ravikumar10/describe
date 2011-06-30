643893f3bcef1ab98410a22e766da74c9bc899fe
sebastien.faure3 <sebastien.faure3@gmail.com>
2011-06-30 07:18:12 +0000

    - Version 1.0.7

--- DEScribe/src/api/utils/appManagement.java
+++ DEScribe/src/api/utils/appManagement.java
@@ -1,6 +1,24 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
+/**
+ *
+DEScribe - A Descriptive Experience Sampling cross platform application
+Copyright (C) 2011
+Sébastien Faure <sebastien.faure3@gmail.com>,
+Bertrand Gros   <gros.bertrand@gmail.com>,
+Yannick Prie    <yannick.prie@univ-lyon1.fr>.
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
  */
 
 package api.utils;
@@ -13,7 +31,7 @@ import java.io.File;
  */
 public class appManagement {
 
-    public static boolean  restartApplication( Object classInJarFile )
+    public static boolean  restartApplication( Object classInJarFile, Boolean isMacReboot )
     {
         String javaBin = System.getProperty("java.home");
 
@@ -32,20 +50,30 @@ public class appManagement {
             return false;
         }
 
-        System.out.println(jarFile.getName());
         /* is it a jar file? */
-        if ( !jarFile.getName().endsWith(".jar") )
-        return false;   //no, it's a .class probably
-
+        if ((!jarFile.getName().endsWith(".jar")) && (!jarFile.getName().endsWith(".exe")) ){
+            return false;   //no, it's a .class probably
+        }
 
         String toExec[]=null;
         if (api.utils.getOs.isWindows()){
-            toExec = new String[] { "\""+javaBin+"\"", "-jar", "\""+jarFile.getPath()+"\"" };
+            //toExec = new String[] { "\""+javaBin+"\"", "-jar", "\""+jarFile.getParent()+"\"" };
+            toExec = new String[] { jarFile.getParent()+"\\" +"DEScribe.exe" };
         } else {
-            toExec = new String[] { javaBin, "-jar", jarFile.getPath()};
+            //toExec = new String[] { javaBin, "-jar", jarFile.getPath()};
+            if (api.utils.getOs.isMac()){
+                //toExec = new String[] { "open","/Applications/DEScribe/DEScribe.app" };
+                if (isMacReboot){
+                    toExec = new String[] { "open","/Applications/DEScribe/DEScribe.app" };
+                } else {
+                toExec = new String[] { javaBin, "-jar", jarFile.getPath(), "reboot"};
+                }
+            } else {
+                toExec = new String[] { javaBin, "-jar", jarFile.getPath()};
+           }
         }
         //String  toExec[] = new String[] { "java", "-jar", jarFile.getPath() };
-        //System.out.println("\""+javaBin+"\""+ " -jar"+" "+ "\""+jarFile.getPath()+"\"" );
+        //System.out.println(jarFile.getParent()+"\\" +"DEScribe.exe" );
         try{
             Process p = Runtime.getRuntime().exec( toExec );
         } catch(Exception e) {
