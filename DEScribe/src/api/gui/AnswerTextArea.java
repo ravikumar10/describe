273910cbd103196c065815a442303e889e91dac2
sebastien.faure3 <sebastien.faure3@gmail.com>
2011-06-30 09:40:22 +0000

    - Forms in askframe.

---
+++ DEScribe/src/api/gui/AnswerTextArea.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package api.gui;
+
+import javax.swing.JTextArea;
+
+/**
+ *
+ * @author Seb
+ */
+public class AnswerTextArea extends JTextArea{
+    public AnswerTextArea(){
+        super();
+    }
+}
---
+++ DEScribe/src/api/gui/AnswerTextField.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package api.gui;
+
+import javax.swing.JTextField;
+
+/**
+ *
+ * @author Seb
+ */
+public class AnswerTextField extends JTextField{
+  public AnswerTextField(){
+      super();
+  }
+}
